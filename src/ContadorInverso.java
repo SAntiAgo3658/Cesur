0178525c15e6d9d98ac043a215cbc70a2604ebf8
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2023-11-14 12:44:59 +0100

    Añadimos otro ejercicio.

--- src/ContadorInverso.java
+++ src/ContadorInverso.java
@@ -1,46 +1,67 @@
+import java.util.Scanner;
 
-public class ContadorInverso { //[1200...620] de 20 en 20 (while, do while y for)
-	
+public class ContadorInverso { // [1200...620] de 20 en 20 (while, do while y for)
+
 	public static void main(String[] args) {
-		
+
 		int x = 1200;
-		
+		int p = 0;
+
+		Scanner teclado = new Scanner(System.in);
+
 		while (x >= 620) {
-			 
+
 			System.out.print(x + " ");
 			x = x - 20;
 		}
-		
+
 		x = 1200;
 		System.out.println();
-		
+
 		do {
-			
+
 			System.out.print(x + " ");
 			x = x - 20;
-		
+
 		} while (x >= 620);
-		
+
 		System.out.println();
-		
+
 		for (int i = 1200; i >= 620; i = i - 20) {
-			
+
 			System.out.print(i + " ");
-			
+
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
+		System.out.println("Introduzca el número máximo");
+		int numMax = teclado.nextInt();
+		int numMin;
+
+		do {
+			System.out.println("Introduzca el número mínimo");
+			numMin = teclado.nextInt();
+
+		} while (numMin >= numMax);
+
+		System.out.println("Introduzca salto deseado");
+		int salto = teclado.nextInt();
+
+		System.out.print("[");
+
+
+		while (numMax >= numMin) {
+			System.out.print(numMax + ", ");
+			numMax = numMax - salto;
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+		do {
+			System.out.print(numMax + ", ");
+			numMax = numMax - salto;
+			
+		} while (numMax >= numMin);
 
+	}
 }
