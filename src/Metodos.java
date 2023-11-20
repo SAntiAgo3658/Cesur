ed2252d354f699d0ef04fa68d6a7fd4c093ad4f2
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2023-11-20 13:27:08 +0100

    Construcción métodos.

---
+++ src/Metodos.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+
+		valoresCorrectos();
+	}
+
+	static boolean comprobarValor(int max, int min, int valor) {
+
+		if ((min > valor) || (max < valor)) {
+			return false;
+
+		}
+		return true;
+
+	}
+
+	static boolean comprobarEntradas(int max, int min) {
+
+
+		if (max < min) {
+			return false;
+
+		}
+		return true;
+	}
+
+	static void valoresCorrectos() {
+		int valor = 0;
+		int max = 0;
+		int min = 0;
+		Scanner teclado = new Scanner(System.in);
+
+		do {
+
+			System.out.println("Introduzca los valores máximo y mínimo; en ese orden y con coherencia.");
+			max = teclado.nextInt();
+			min = teclado.nextInt();
+
+		} while (!comprobarEntradas(max, min));
+
+		do {
+			System.out.println("Introduzca un valor correcto.");
+			valor = teclado.nextInt();
+
+		} while (!comprobarValor(max, min, valor));
+
+		System.out.println("el valor " + " entre " + min + " y " + max);
+		System.out.println(comprobarValor(max, min, valor));
+
+		teclado.close();
+
+		
+	}
+}
