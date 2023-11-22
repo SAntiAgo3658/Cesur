236428f9dd19788c96c297686746dc2eb7408eba
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2023-11-22 13:28:43 +0100

    nuevos

---
+++ src/Arrays.java
@@ -0,0 +1,15 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		int[] listaInteger = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			listaInteger[i] = i;
+			System.out.println(listaInteger[i]);
+
+		}
+	}
+
+}
---
+++ src/Impuestos.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Impuestos {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+		int ingresos = 0;
+		int aPagar = 0;
+		int tipo = 0;
+
+		
+			do {
+				System.out.println("Introduzca sus ingresos");
+				ingresos = teclado.nextInt();
+
+			} while (ingresos < 1);
+			
+			if (ingresos < 30001) {
+				tipo = 10;
+			
+			} else if ((ingresos > 30000) && (50001 > ingresos)) {
+				tipo = 15;
+				
+			} else {
+				tipo = 20;
+			}
+			
+			System.out.println(ingresos * tipo / 100 + " " + tipo + "%");
+			
+
+		
+
+	}
+}
