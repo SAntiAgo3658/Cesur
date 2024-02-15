cbe9d3b6635f62b2d75d5e54efdb752421f46be5
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2024-02-15 12:04:59 +0100

    Ficheros

--- src/practicas/EjemploFicheros.java
+++ src/practicas/EjemploFicheros.java
@@ -2,6 +2,7 @@ package practicas;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class EjemploFicheros {
 
@@ -10,11 +11,18 @@ public class EjemploFicheros {
 		// leer de un fichero caracter a caracter y lo muestro por pantalla
 
 		FileInputStream entrada = null;
-		
+		FileOutputStream salida = null;
 		
 
 		try {
-			entrada = new FileInputStream("PruebaFichero.txt");
+			entrada = new FileInputStream("Prueba Fichero.txt");
+			int ascii;
+			ascii = entrada.read();
+			while (ascii != -1) {
+				System.out.print((char) ascii);
+				ascii = entrada.read();
+
+			}
 
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
@@ -26,6 +34,25 @@ public class EjemploFicheros {
 			}
 
 		}
+		
+		try {
+			salida = new FileOutputStream("Prueba Fichero.txt");
+			salida.write(72);
+	
+
+			
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (salida != null) {
+				salida.close();
+
+			}
+
+		}
+
 
 	}
 
---
+++ src/practicas/EjemplosFicheros2.java
@@ -0,0 +1,72 @@
+package practicas;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class EjemplosFicheros2 {
+
+	public static void main(String[] args) throws IOException {
+
+		// pedir una frase por pantalla e intoducirla en el fichero: Prueba Fichero.txt
+		// mostrar por pantalla el contenido del fichero
+
+		FileInputStream entrada = null;
+		FileOutputStream salida = null;
+		Scanner teclado = new Scanner(System.in);
+		String contenido;
+		char[] guarda = null;
+
+		System.out.println("Introduce el contenido");
+		contenido = teclado.nextLine();
+		guarda = new char[contenido.length()];
+
+		for (int i = 0; i < guarda.length; i++) {
+			guarda[i] = contenido.charAt(i);
+
+		}
+
+		try {
+			salida = new FileOutputStream("Prueba Fichero.txt");
+
+			for (int i = 0; i < guarda.length; i++) {
+				salida.write((int) guarda[i]);
+
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (salida != null) {
+				salida.close();
+
+			}
+
+		}
+
+		try {
+			entrada = new FileInputStream("Prueba Fichero.txt");
+			int ascii;
+			ascii = entrada.read();
+			while (ascii != -1) {
+				System.out.print((char) ascii);
+				ascii = entrada.read();
+
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (entrada != null) {
+				entrada.close();
+
+			}
+
+		}
+
+	}
+
+}
