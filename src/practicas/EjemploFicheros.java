d2ecdb53820b80434c79aa443b41b5caad26b531
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2024-02-19 12:19:13 +0100

    clase 19/02/2024

--- src/practicas/EjemploFicheros.java
+++ src/practicas/EjemploFicheros.java
@@ -1,30 +1,47 @@
 package practicas;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 public class EjemploFicheros {
 
 	public static void main(String[] args) throws IOException {
 
-		// leer de un fichero caracter a caracter y lo muestro por pantalla
+		// Trabajar con caracteres
+		// Lectura de un fichero: "Prueba fichero.txt"
+		// Escritura de un fichero:
+		// Escritura de un fichero, al final del mismo.
 
-		FileInputStream entrada = null;
-		FileOutputStream salida = null;
-		
+		// Flujo de lectura:
+		FileReader entrada = null;
+		boolean nofin = true;
+		char caracter;
 
 		try {
-			entrada = new FileInputStream("Prueba Fichero.txt");
-			int ascii;
-			ascii = entrada.read();
-			while (ascii != -1) {
-				System.out.print((char) ascii);
-				ascii = entrada.read();
+			entrada = new FileReader("Prueba fichero.txt");
 
-			}
+			do {
+				caracter = (char) entrada.read();
 
-		} catch (IOException e) {
+				if (caracter == (char) -1) {
+					nofin = false;
+
+				} else {
+
+					System.out.print(caracter);
+
+				}
+
+			} while (nofin);
+
+		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 
 		} finally {
@@ -33,16 +50,26 @@ public class EjemploFicheros {
 
 			}
 
+		} // Fin try-catch
+
+		FileOutputStream salida = null;
+		String frase = "esto es una prueba de escritura en fichero";
+		char[] fraseArray = new char[frase.length()];
+
+		for (int i = 0; i < frase.length(); i++) {
+			fraseArray[i] = frase.charAt(i);
+
 		}
-		
+
 		try {
-			salida = new FileOutputStream("Prueba Fichero.txt");
-			salida.write(72);
-	
+			salida = new FileOutputStream("Prueba fichero.txt");
 
-			
+			for (int i = 0; i < fraseArray.length; i++) {
+				salida.write((int) fraseArray[i]);
 
-		} catch (IOException e) {
+			}
+
+		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 
 		} finally {
@@ -51,8 +78,80 @@ public class EjemploFicheros {
 
 			}
 
-		}
+		} // Final try-catch
 
+		System.out.println();
+		System.out.println();
+
+		FileWriter fw = null;
+		Scanner teclado = new Scanner(System.in);
+
+		try {
+			fw = new FileWriter("Prueba fichero.txt", true); // opción append habilitada!
+			String meteFrase;
+			fw.write((int) '\n');
+
+			do {
+				System.out.println("Introduzca una frase");
+				meteFrase = teclado.nextLine();
+				System.out.println(meteFrase);
+
+				if (meteFrase.length() > 0) {
+					for (int i = 0; i < meteFrase.length(); i++) {
+						fw.write(meteFrase.charAt(i));
+
+					}
+
+				}
+
+				fw.write((int) '\n');
+
+			} while (((meteFrase.charAt(0) != 'Q') || (meteFrase.length() != 1)) && (meteFrase.length() > 0));
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (fw != null) {
+				fw.close();
+
+			}
+
+		}
+		
+		FileReader entrada2 = null;
+		boolean nofin2 = true;
+		char caracter2;
+
+		try {
+			entrada2 = new FileReader("Prueba fichero.txt");
+
+			do {
+				caracter2 = (char) entrada2.read();
+
+				if (caracter2 == (char) -1) {
+					nofin2 = false;
+
+				} else {
+
+					System.out.print(caracter2);
+
+				}
+
+			} while (nofin2);
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (entrada2 != null) {
+				entrada2.close();
+
+			}
+
+		} // Fin try-catch
+
+		teclado.close();
 
 	}
 
--- src/practicas/EjemplosFicheros2.java
+++ src/practicas/EjemplosFicheros2.java
@@ -67,6 +67,8 @@ public class EjemplosFicheros2 {
 
 		}
 
+		teclado.close();
+
 	}
 
 }
