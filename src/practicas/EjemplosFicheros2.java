159f666180cc00189f9545816d6587b329c634e1
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2024-02-22 10:18:03 +0100

    22/02/24 2.0

--- src/practicas/EjemplosFicheros2.java
+++ src/practicas/EjemplosFicheros2.java
@@ -2,32 +2,60 @@ package practicas;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Scanner;
 
 public class EjemplosFicheros2 {
 
 	public static void main(String[] args) throws IOException {
 
-		File fichero = null;
-		fichero = new File("pruebaFile");
-		System.out.println(fichero.getAbsolutePath());
-		System.out.println(fichero.getCanonicalPath());
+		RandomAccessFile fichero = null;
+		try {
+			fichero = new RandomAccessFile("ficheroAleatorio.txt", "rw");
 
-		File miDirectorio = null;
-		miDirectorio = new File("C:\\Users\\SantiagoElíasDavidGo\\eclipse-workspace\\DAMCesur");
-		File[] miLista = miDirectorio.listFiles();
+			for (int i = 0; i < 26; i++) {
+				fichero.writeChar(97 + i);
 
-		for (int i = 0; i < miLista.length; i++) {
+			}
 
-			if (miLista[i].isDirectory()) {
-				System.out.println("DIRECTORIO:     " + miLista[i].getName());
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
 
-			} else {
-				System.out.println(miLista[i].getName());
+		} finally {
+			if (fichero != null)
+				fichero.close();
+
+		}
+
+		try {
+			fichero = new RandomAccessFile("ficheroAleatorio.txt", "r");
+
+			for (int i = 0; i < 26; i++) {
+				System.out.print("[" + fichero.readChar() + " " +  fichero.getFilePointer() + "]" + " ");
+
+			}
+			
+			System.out.println("\nEl puntero acaba en la posición " + fichero.getFilePointer());
+			fichero.seek(0);
+			 
+			for (int i = 0; i < 26 ; i++) {
+				fichero.skipBytes(1);
+				System.out.print("[" + (char) fichero.readByte() + " " +  fichero.getFilePointer() + "]" + " ");
 				
 			}
+			
+			System.out.println("\nEl puntero acaba en la posición " + fichero.getFilePointer());
+	
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+
+		} finally {
+			if (fichero != null)
+				fichero.close();
 
 		}
 
